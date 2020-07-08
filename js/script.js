d13246fc971aa9fe32fe14949678fe7879184337
JayJaber <62089050+JayJaber@users.noreply.github.com>
2020-07-07 23:22:14 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var today = new Date().toISOString().split('T')[0];
 document.getElementsByName("setTodaysDate")[0].setAttribute('min', today);
 
-function myFunction() {
-  alert("We are still under full operation and continue to serve the bay are during our lockdown. This guidance provides recommendations on the cleaning and disinfection of households where persons under investigation (PUI) or those with confirmed COVID-19 reside or may be in self- isolation. It is aimed at limiting the survival of the virus in the environments. These recommendations will be updated if additional information becomes available.  These guidelines are focused on household settings and are meant for the general public.  Cleaning refers to the removal of germs, dirt, and impurities from surfaces. It does not kill germs, but by removing them, it lowers their numbers and the risk of spreading infection. Disinfecting refers to using chemicals, for example, EPA-registered disinfectants, to kill germs on surfaces. This process does not necessarily clean dirty surfaces or remove germs, but by killing germs on a surface after cleaning, it can further lower the risk of spreading infection.");
+
+function alertFunction() {
+  alert("Important Update:\nWe are still under full operation and continue to serve our clients during the shelter-in-place order. Our regular hours are Monday through Saturday, 8am to 6pm.\n\nWhat you can expect from us:\nWorkers in full protective gear, sanitization of all contantacted surfaces, smaller groups of cleaners to prevent the spread of virus.");
 }
