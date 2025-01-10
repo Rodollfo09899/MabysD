20f18a5b91f10aafff08284f3f5beb41ceba1257
Rodollfo09899 <58276692+Rodollfo09899@users.noreply.github.com>
2025-01-10 11:26:33 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,3 @@ var today = new Date().toISOString().split('T')[0];
 document.getElementsByName("setTodaysDate")[0].setAttribute('min', today);
 
 
-function alertFunction() {
-  alert("Important Update:\nWe are still under full operation and continue to serve our clients during the shelter-in-place order. Our regular hours are Monday through Saturday, 8am to 6pm.\n\nWhat you can expect from us:\nWorkers in full protective gear, sanitization of all contantacted surfaces, smaller groups of cleaners to prevent the spread of virus.");
-}
